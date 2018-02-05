bc44d206c00387e64c394ef6a23fd6d04b39756a
drminnaar <douglas.minnaar@gmail.com>
2018-02-05 09:25:08 +0200

    Amend 'Display' component to use correct PropType for children

--- src/components/Timer/Display.js
+++ src/components/Timer/Display.js
@@ -20,22 +20,22 @@ const Display = (props) => {
         <div className="display" style={{ position: 'relative' }}>
             {
                 props.status === 'started'
-            && <div className="display-time align-self-center" style={runningDisplayStyle}>
-                {time.getTime(props.time)}
-            </div>
+                && <div className="display-time align-self-center" style={runningDisplayStyle}>
+                    {time.getTime(props.time)}
+                </div>
             }
             {
                 props.status !== 'started' &&
-            <div className="d-flex flex-column">
-                <div className="text-info h6 align-self-center" style={{position: 'absolute', top: 60}}>
-                    {time.getTime(props.time)}
+                <div className="d-flex flex-column">
+                    <div className="text-info h6 align-self-center" style={{ position: 'absolute', top: 60 }}>
+                        {time.getTime(props.time)}
+                    </div>
+                    <input className="display-time align-self-center" style={{ position: 'absolute', top: 100 }}
+                        maxLength="6" value={props.seconds}
+                        onChange={onChange} />
                 </div>
-                <input className="display-time align-self-center" style={{position: 'absolute', top: 100}}
-                    maxLength="6" value={props.seconds}
-                    onChange={onChange} />
-            </div>                            
             }
-            <div style={{ position: 'absolute', bottom: 0, left: '-10px'}}>                            
+            <div style={{ position: 'absolute', bottom: 0, left: '-10px' }}>
                 {props.children}
             </div>
         </div>
@@ -50,7 +50,7 @@ Display.defaultProps = {
 };
 
 Display.propTypes = {
-    children: PropTypes.children,
+    children: PropTypes.element,
     seconds: PropTypes.number.isRequired,
     status: PropTypes.string,
     time: PropTypes.number,
